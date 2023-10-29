83873b08cfe3b16001f874a749a5ac3c1ed6da7f
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-10-29 16:35:47 +0900

    실전! QueryDsl - 사용자 정의 repository

--- src/test/java/study/querydsl/repository/SearchRepositoryTest.java
+++ src/test/java/study/querydsl/repository/SearchRepositoryTest.java
@@ -14,32 +14,15 @@ import study.querydsl.entity.Team;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class MemberRepositoryTest {
-
+class SearchRepositoryTest {
   @PersistenceContext
   EntityManager em;
 
   @Autowired
-  MemberRepository memberRepository;
-
-  @Test
-  void basicTest() {
-    Member member = new Member("member1", 10);
-    memberRepository.save(member);
-
-    Member findMember = memberRepository.findById(member.getId()).get();
-    assertThat(findMember).isEqualTo(member);
-
-    List<Member> result = memberRepository.findAll();
-    assertThat(result).containsExactly(member);
-
-    List<Member> result1 = memberRepository.findByUsername("member1");
-    assertThat(result1).containsExactly(member);
-  }
+  SearchRepository searchRepository;
 
   @Test
   void search() {
@@ -64,9 +47,8 @@ class MemberRepositoryTest {
     condition.setAgeLoe(40);
     condition.setTeamName("teamB");
 
-    List<MemberTeamDto> result = memberRepository.search(condition);
+    List<MemberTeamDto> result = searchRepository.search(condition);
 
     assertThat(result).extracting("username").containsExactly("member4");
   }
-
 }
